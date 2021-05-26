82cb71f12c86c1d8899200cd3d8b9495da6e60c6
GitterJakub <80820885+GitterJakub@users.noreply.github.com>
2021-05-26 11:02:27 +0200

    Shall add Calculator class and a Method returning the sum

---
+++ src/main/java/ch/bbw/Calculator.java
@@ -0,0 +1,15 @@
+package ch.bbw;
+
+/***
+ * @version 26.05.21
+ * @author Jakub Baranec
+ * addiert zwei Zahlen
+ */
+
+
+
+public class Calculator {
+    public int sum(int summand1, int summand2){
+        return summand1 + summand2;
+    }
+}
